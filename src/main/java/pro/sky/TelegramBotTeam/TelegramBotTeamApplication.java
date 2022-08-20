97ec9e263d47f2ae4c7c841403ee82a57acd98f2
AlbusCat <olegkveko@gmail.com>
2022-08-20 09:47:27 +0700

    Initial commit

---
+++ src/main/java/pro/sky/TelegramBotTeam/TelegramBotTeamApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.TelegramBotTeam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramBotTeamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelegramBotTeamApplication.class, args);
+	}
+
+}
