1dced435499bdf44343397c6ffe9d1d6c9a8e4a3
AlbusCat <olegkveko@gmail.com>
2022-08-22 22:42:25 +0700

    Шаблон телеграм бота

--- src/main/java/pro/sky/TelegramBotTeam/TelegramBotTeamApplication.java
+++ src/main/java/pro/sky/TelegramBotTeam/TelegramBotTeamApplication.java
@@ -2,8 +2,11 @@ package pro.sky.TelegramBotTeam;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
 @SpringBootApplication
+@EnableScheduling
 public class TelegramBotTeamApplication {
 
 	public static void main(String[] args) {
