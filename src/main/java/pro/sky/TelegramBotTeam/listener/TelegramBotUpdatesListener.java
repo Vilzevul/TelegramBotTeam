1c66b546b456945213f14f1abfc5fe5ed949c011
AlbusCat <olegkveko@gmail.com>
2022-08-23 16:18:44 +0700

    Шаблон телеграм бота + Клавиатура и инлайн меню

--- src/main/java/pro/sky/TelegramBotTeam/listener/TelegramBotUpdatesListener.java
+++ src/main/java/pro/sky/TelegramBotTeam/listener/TelegramBotUpdatesListener.java
@@ -2,24 +2,38 @@ package pro.sky.TelegramBotTeam.listener;
 
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
+import com.pengrad.telegrambot.model.CallbackQuery;
+import com.pengrad.telegrambot.model.Message;
 import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.model.User;
+import com.pengrad.telegrambot.model.request.ParseMode;
 import com.pengrad.telegrambot.request.SendMessage;
 import com.pengrad.telegrambot.response.SendResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import pro.sky.TelegramBotTeam.KeyBoardButton;
 
 import javax.annotation.PostConstruct;
+import java.io.IOException;
 import java.util.List;
 
 @Service
 public class TelegramBotUpdatesListener implements UpdatesListener {
-
+    private final KeyBoardButton keyBoardButton;
     private Logger logger = LoggerFactory.getLogger(TelegramBotUpdatesListener.class);
+    String text, btnCommand;
+    Long userId;
+    User user;
+    Message message;
 
     @Autowired
     private TelegramBot telegramBot;
+
+    public TelegramBotUpdatesListener(KeyBoardButton keyBoardButton) {
+        this.keyBoardButton = keyBoardButton;
+    }
 
     @PostConstruct
     public void init() {
@@ -27,21 +41,51 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
     }
 
     @Override
-    public int process(List<Update> updates) {
+    public int process(List<Update> updates) throws RuntimeException {
         updates.forEach(update -> {
             logger.info("Processing update: {}", update);
-            // Process your updates here
+            // после нажатия на кнопки
+            CallbackQuery callbackQuery = update.callbackQuery();
+            if (callbackQuery != null) {
+                text = btnCommand = callbackQuery.data();
+                user = callbackQuery.from();
+                userId = user.id();
 
-
-            if (update.message() != null) {
-                String message = update.message().text();
-                logger.info("- Processing telegramBot() - " + message);
-                if (message.equals("/start")) {
-                    SendResponse response = telegramBot.execute(new SendMessage(update.message().from().id(),
-                            update.message().from().firstName() + ", hello!"));
-                }
+                logger.info("- Processing telegramBot() - " + btnCommand);
             }
 
+            //После ввода команды
+            message = update.message();
+            if (message != null) {
+                user = message.from();
+                userId = user.id();
+                btnCommand = text = message.text();
+                logger.info("- Processing telegramBot() - " + text);
+            }
+            text = keyBoardButton.getText(btnCommand);
+            try {
+
+                if (text.equals("/start")) {
+                    SendResponse response = telegramBot.execute(new SendMessage(message.from().id(),
+                            message.from().firstName() + ", hello!" + "\n" + keyBoardButton.getText("text"))
+                            .parseMode(ParseMode.HTML)
+                            .replyMarkup(keyBoardButton.getMainKeyboardMarkup()));
+                    return;
+                }
+
+
+                telegramBot.execute(new SendMessage(userId, text)
+                        .replyMarkup(keyBoardButton.getMainKeyboardMarkup())
+                        //       .replyMarkup(keyboard)
+                        .replyMarkup(keyBoardButton.getInlineKeyboard(btnCommand))
+                        //       .replyMarkup(replyKeyboardRemove)
+                        .parseMode(ParseMode.HTML));
+
+            } catch (RuntimeException e) {
+                return;
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
         });
         return UpdatesListener.CONFIRMED_UPDATES_ALL;
     }
