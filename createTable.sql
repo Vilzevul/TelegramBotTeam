838ab918c1926ec41daf5377812aa77e861b808a
katya <kotenokches@rambler.ru>
2022-08-27 09:46:32 +0500

    Долгова - Удалены 2 таблицы Волонтеры и Усыновители. Изменила таблицу users - добавила столбец role (1-пользователь, 2-усыновитель, 3-волонтер)

--- createTable.sql
+++ createTable.sql
@@ -5,7 +5,8 @@ CREATE TABLE users
     idChat   integer     NOT NULL,
     nameUser varchar(36) NOT NULL,
     phone    integer,
-    idMenu   integer
+    idMenu   integer,
+    role     integer
 );
 --Создание таблицы pets "Питомцы"
 CREATE TABLE pets
@@ -27,24 +28,9 @@ CREATE TABLE keepingPets
     wellBeing varchar,
     behavior  varchar
 );
---Создание таблицы parentPets "Усыновитель питомца"
-CREATE TABLE parentPets
-(
-    id        integer PRIMARY KEY,
-    idPets    integer NOT NULL,
-    idUsers   integer NOT NULL,
-    fio      varchar,
-    mail      varchar(36)
-);
 --Создание таблицы directory "Справочник"
 CREATE TABLE directory
 (
     id        integer PRIMARY KEY,
     name      text
-);
---Создание таблицы volunteers "Волонтеры"
-CREATE TABLE volunteers
-(
-    id        integer PRIMARY KEY,
-    fio      varchar
 );
