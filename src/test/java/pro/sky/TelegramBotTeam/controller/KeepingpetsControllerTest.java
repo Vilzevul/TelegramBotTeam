4cc5d25ee55801b04cbd97cdbe2cd398e38373e5
L <EBogatyrevaPK@yandex.ru>
2022-09-02 18:25:01 +1200

    1. создание тестового класса KeepingpetsControllerTest

---
+++ src/test/java/pro/sky/TelegramBotTeam/controller/KeepingpetsControllerTest.java
@@ -0,0 +1,43 @@
+package pro.sky.TelegramBotTeam.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import pro.sky.TelegramBotTeam.repository.KeepingpetsRepository;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class KeepingpetsControllerTest {
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private KeepingpetsController keepingpetsController;
+
+    @MockBean
+    private KeepingpetsRepository keepingpetsRepository;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    private static final ObjectMapper om = new ObjectMapper();
+    //ObjectMapper - это класс в пакете jackson-databind,
+    //который предоставляет функцию чтения и записи JSON, может легко преобразовывать объекты и JSON.
+
+    @Test
+    void contextLoads() throws Exception {
+        Assertions.assertThat(keepingpetsController).isNotNull();
+    }
+
+    @Test
+    void getUsersWithReport() throws Exception {
+        Assertions
+                .assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/reports/users_report", String.class))
+                .isNotNull();
+
+    }
+}
