9df4456bcad43c22455181dc2f5ebf9d4eab70ea
L <EBogatyrevaPK@yandex.ru>
2022-09-19 20:32:30 +1200

    1. контроллер перименован в TelegramBotController

---
+++ src/main/java/pro/sky/telegramBotTeam/controller/TelegramBotController.java
@@ -0,0 +1,59 @@
+package pro.sky.telegramBotTeam.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
+import pro.sky.telegramBotTeam.model.Report;
+import pro.sky.telegramBotTeam.service.*;
+
+import java.time.LocalDate;
+
+@RestController
+@RequestMapping("/reports")
+@SuppressWarnings("unused")
+public class TelegramBotController {
+    private final UsersService usersService;
+    private final ReportService reportService;
+
+    private final AdoptionService adoptionService;
+
+    private final MemberService memberService;
+
+    private final ShelterService shelterService;
+
+    private static final Logger log = LoggerFactory.getLogger(TelegramBotController.class);
+
+    public TelegramBotController(UsersService usersService, ReportService reportService, AdoptionService adoptionService, MemberService memberService, ShelterService shelterService) {
+        this.usersService = usersService;
+        this.reportService = reportService;
+        this.adoptionService = adoptionService;
+        this.memberService = memberService;
+        this.shelterService = shelterService;
+    }
+
+    @GetMapping
+    public String testAPI() {
+        return "Web API is working";
+    }
+
+    /**
+     * Возвращает список пользователей и связанных с ними отчетами
+     */
+    @GetMapping(path = "/users_report")
+    public Report getUsersWithReport(@RequestParam(required = true) Long idAdoption, LocalDate date) {
+        if (idAdoption > 0)
+            log.debug("Method - getUsersWithReport was called");
+            return reportService.getReport(idAdoption, date);
+    }
+    @PutMapping(path = "/update_reportById")
+    public Report updateUsersWithReport(@RequestParam(required = true) Long idAdoption, LocalDate date) {
+
+
+        if (idAdoption > 0)
+            log.debug("Method - getUsersWithReport was called");
+        return reportService.getReport(idAdoption, date);
+    }
+
+
+
+}
